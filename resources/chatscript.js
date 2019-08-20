b3aa8625ff9dcafab7cae15fa7c4ac05a7d25343
BlueJay <thechosenone113@outlook.com>
2019-08-20 18:24:17 +1000

    Update chatscript.js

--- resources/chatscript.js
+++ resources/chatscript.js
@@ -118,6 +118,7 @@ function updateMembersDOM() {
 function createMessageElement(text, member) {
   const el = document.createElement('div');
   el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(":"));
   el.appendChild(document.createTextNode(text));
   el.className = 'message';
   return el;
