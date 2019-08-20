5944440a219246d345d6a6050b469c7d33586d7c
BlueJay <thechosenone113@outlook.com>
2019-08-20 18:27:51 +1000

    Update chatscript.js

--- resources/chatscript.js
+++ resources/chatscript.js
@@ -101,7 +101,7 @@ function sendMessage() {
 function createMemberElement(member) {
   const { name, color } = member.clientData;
   const el = document.createElement('div');
-  el.appendChild(document.createTextNode(name + ":"));
+  el.appendChild(document.createTextNode(name));
   el.className = 'member';
   el.style.color = color;
   return el;
