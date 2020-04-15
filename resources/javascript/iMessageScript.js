23d835f3d9fb97874dabf3fb3259d5e6fbed2b84
BlueJay113 <thechosenone113@outlook.com>
2020-04-15 17:21:14 +1000

    Update iMessageScript.js

--- resources/javascript/iMessageScript.js
+++ resources/javascript/iMessageScript.js
@@ -168,7 +168,6 @@ function responsiveChatPush(element, sender, origin, date, message) {
 			},
 		});
 	}
-	if (message == "sUpErSeCrEtMiNiMiZeCoDe") { minimizeTrigger = 1 }
 }
 
 if (Push.Permission.has())
